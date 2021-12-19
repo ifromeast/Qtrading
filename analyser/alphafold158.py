51e1e7d41a299e7c71d008eb8833801615b79152
juliu <juliu@pinduoduo.com>
2021-12-19 22:53:42 +0800

    topkdrop

--- analyser/alphafold158.py
+++ analyser/alphafold158.py
@@ -2,24 +2,26 @@ import qlib
 from qlib.config import REG_CN
 from qlib.contrib.data.handler import Alpha158
 
-qlib.init(provider_uri='data/cn_data', region=REG_CN)
 
-data_handler_config = {
-    "start_time": "2018-01-01",
-    "end_time": "2021-08-01",
-    "fit_start_time": "2018-01-01",
-    "fit_end_time": "2012-12-31",
-    "instruments": "csi300",
-}
+if __name__ == '__main__':
+    qlib.init(provider_uri='data/cn_data', region=REG_CN)
 
-h = Alpha158(**data_handler_config)
+    data_handler_config = {
+        "start_time": "2018-01-01",
+        "end_time": "2021-08-01",
+        "fit_start_time": "2018-01-01",
+        "fit_end_time": "2012-12-31",
+        "instruments": "csi300",
+    }
 
-# get all the columns of the data
-print('columns: \n', h.get_cols())
+    h = Alpha158(**data_handler_config)
 
-# fetch all the labels
-print('labels: \n', h.fetch(col_set="label"))
+    # get all the columns of the data
+    print('columns: \n', h.get_cols())
 
-# fetch all the features
-print('features: \n', h.fetch(col_set="feature"))
+    # fetch all the labels
+    print('labels: \n', h.fetch(col_set="label"))
+
+    # fetch all the features
+    print('features: \n', h.fetch(col_set="feature"))
 
